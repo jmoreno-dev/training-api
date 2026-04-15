ab245e0228917e16695d018a986b079bce37dbfe
José Antonio Moreno Marín <ytanonim12@gmail.com>
2026-04-15 19:04:05 +0200

    feat: add unit tests for User entity getters and setters

---
+++ src/test/java/com/jmorenodev/training_api/model/UserTest.java
@@ -0,0 +1,38 @@
+package com.jmorenodev.training_api.model;
+
+import com.jmorenodev.training_api.model.enums.Role;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+
+    @Test
+    void shouldCreateUserAndVerifyGettersAndSetters(){
+         User trainer= new User();
+         User newTrainer = new User();
+         User user= new User(1L, "testName", "testSurname", "email@email.com", "12345678", Role.ROLE_CLIENT, trainer);
+
+         // Getters test
+         assertEquals(1L, user.getId());
+         assertEquals("testName", user.getName());
+         assertEquals("testSurname", user.getSurname());
+         assertEquals("email@email.com", user.getEmail());
+         assertEquals("12345678", user.getPassword());
+         assertEquals(Role.ROLE_CLIENT, user.getRole());
+         assertEquals(trainer, user.getTrainer());
+
+         //Setters test
+        user.setName("newName");
+        assertEquals("newName", user.getName());
+        user.setSurname("newSurname");
+        assertEquals("newSurname", user.getSurname());
+        user.setEmail("newEmail@email.com");
+        assertEquals("newEmail@email.com", user.getEmail());
+        user.setPassword("012345678");
+        assertEquals("012345678", user.getPassword());
+        user.setRole(Role.ROLE_TRAINER);
+        assertEquals(Role.ROLE_TRAINER, user.getRole());
+        user.setTrainer(newTrainer);
+        assertEquals(newTrainer, user.getTrainer());
+    }
+}
