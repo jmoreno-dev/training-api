c542ed8bd8385c07af450148a336c47d04780f78
jmoreno-dev <ytanonim12@gmail.com>
2026-04-16 10:23:40 +0200

    corrected Exercise.java

---
+++ src/main/java/com/jmorenodev/training_api/model/Exercise.java
@@ -0,0 +1,32 @@
+package com.jmorenodev.training_api.model;
+
+import com.jmorenodev.training_api.model.enums.Muscles;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table (name = "exercises")
+public class Exercise {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String name;
+
+    @NotBlank
+    @Size (min = 2, max = 100)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Muscles muscleGroup;
+}
