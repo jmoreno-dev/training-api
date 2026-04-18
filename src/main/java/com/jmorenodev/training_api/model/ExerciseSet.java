214665d26a9cd78505bdecc225b8f95fb380198c
José Antonio Moreno Marín <ytanonim12@gmail.com>
2026-04-18 15:43:23 +0200

    feat: add ExerciseSet model with fields for set number, weight, reps, and relationships

---
+++ src/main/java/com/jmorenodev/training_api/model/ExerciseSet.java
@@ -0,0 +1,49 @@
+package com.jmorenodev.training_api.model;
+
+import com.jmorenodev.training_api.model.enums.Rpe;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table (name = "exercise_sets")
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExerciseSet {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer setNumber;
+
+    @PositiveOrZero
+    private double weight;
+
+    @NotNull
+    private Integer reps;
+
+    @Enumerated (EnumType.STRING)
+    private Rpe rpe;
+
+    @Column (name = "rest_seconds")
+    private double restSeconds;
+
+    @ManyToOne
+    @JoinColumn(name = "exercise_id")
+    @NotNull
+    private Exercise exercise;
+
+    @ManyToOne
+    @JoinColumn(name = "session_id")
+    @NotNull
+    private Session session;
+}
