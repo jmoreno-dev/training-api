5a92bf833671988896bf7c52899cbd9e6f2e8c7b
José Antonio Moreno Marín <ytanonim12@gmail.com>
2026-04-18 15:58:48 +0200

    feat: refactor Routine model to use Lombok annotations for getter and setter methods

--- src/main/java/com/jmorenodev/training_api/model/Routine.java
+++ src/main/java/com/jmorenodev/training_api/model/Routine.java
@@ -3,15 +3,15 @@ package com.jmorenodev.training_api.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 @Table (name = "routines")
+
 public class Routine {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
