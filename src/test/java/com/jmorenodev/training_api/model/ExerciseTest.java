472490cc7e7e76087131e0023c678b8a1142c667
José Antonio Moreno Marín <ytanonim12@gmail.com>
2026-04-20 19:02:36 +0200

    feat: add validation tests for Exercise model to ensure name and description constraints

--- src/test/java/com/jmorenodev/training_api/model/ExerciseTest.java
+++ src/test/java/com/jmorenodev/training_api/model/ExerciseTest.java
@@ -1,12 +1,84 @@
 package com.jmorenodev.training_api.model;
 
+import com.jmorenodev.training_api.model.enums.Muscles;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExerciseTest {
 
+    private Validator validator;
+    private Exercise validExercise;
+
     @BeforeEach
     void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
+        validExercise = new Exercise(1L, "name", "description", Muscles.BICEPS);
+    }
+
+    @Test
+    void shouldCreateExercise(){
+        assertNotNull(validExercise);
+
+        Set<ConstraintViolation<Exercise>> violations = validator.validate(validExercise);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void shouldFailWithNameShorterThanTwoLetters(){
+        validExercise.setName("a");
+
+        Set<ConstraintViolation<Exercise>> violations = validator.validate(validExercise);
+
+        assertFalse(violations.isEmpty());
+        assertEquals(1, violations.size());
+        assertEquals("name", violations.iterator().next().getPropertyPath().toString());
+    }
+
+    @Test
+    void shouldFailWithNameLargerThanOneHundredLetters(){
+        String invalidName = "a";
+        String newName = invalidName.repeat(101);
+        validExercise.setName(newName);
+
+        Set<ConstraintViolation<Exercise>> violations = validator.validate(validExercise);
+
+        assertFalse(violations.isEmpty());
+        assertEquals(1, violations.size());
+        assertEquals("name", violations.iterator().next().getPropertyPath().toString());
+    }
+
+    @Test
+    void shouldFailWithDescriptionShorterThanTwoLetters(){
+        validExercise.setDescription("b");
+
+        Set<ConstraintViolation<Exercise>> violations = validator.validate(validExercise);
+
+        assertFalse(violations.isEmpty());
+        assertEquals(1, violations.size());
+        assertEquals("description", violations.iterator().next().getPropertyPath().toString());
+    }
+
+    @Test
+    void shouldFailWithDescriptionLargerThanOneHundredLetters(){
+        String invalidDescription = "b";
+        String newDescription = invalidDescription.repeat(101);
+        validExercise.setDescription(newDescription);
+
+        Set<ConstraintViolation<Exercise>> violations = validator.validate(validExercise);
+
+        assertFalse(violations.isEmpty());
+        assertEquals(1, violations.size());
+        assertEquals("description", violations.iterator().next().getPropertyPath().toString());
     }
 }
