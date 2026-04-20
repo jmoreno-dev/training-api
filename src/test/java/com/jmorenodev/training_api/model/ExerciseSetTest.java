adc98c5700d780592c7c2d68487118591539dc08
José Antonio Moreno Marín <ytanonim12@gmail.com>
2026-04-20 20:06:15 +0200

    feat: initialize Routine instance in ExerciseSetTest setup for improved test coverage

--- src/test/java/com/jmorenodev/training_api/model/ExerciseSetTest.java
+++ src/test/java/com/jmorenodev/training_api/model/ExerciseSetTest.java
@@ -19,13 +19,13 @@ class ExerciseSetTest {
 
     private Validator validator;
     private ExerciseSet validExerciseSet;
-    private Routine routine;
 
     @BeforeEach
     void setUp() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
         User user = new User(1L, "name", "surname", "email@email.com", "12345678", Role.ROLE_CLIENT, null);
+        Routine routine =  new Routine(1L, "name", user);
 
         Exercise exercise = new Exercise(1L, "coercion", "description", Muscles.ABS);
         Session session = new Session(1L, LocalDate.now(), user, routine);
